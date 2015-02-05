3596f1e8644d694a98e14f7d01f3715312bded69
Diego Sarro <diegosarro@gmail.com>
2015-02-05 21:28:02 -0200

    Se sube el archivo prueba.py y se actualiza el de test.py

---
+++ prueba.py
@@ -0,0 +1,26 @@
+
+class Prueba:
+
+    def Prueba(self,funcionalidad=None,objetivo=None):
+
+        self.funcionalidad = funcionalidad
+        self.objetivo = objetivo
+
+
+    def getFuncionalidad(self):
+
+        return self.funcionalidad
+
+    def setFuncionalidad(self,pFuncionalidad):
+
+        self.funcionalidad = pFuncionalidad
+
+    def getObjetivo(self):
+
+        return self.objetivo
+
+    def setObjetivo(self,pObjetivo):
+
+        self.objetivo = pObjetivo
+
+    
